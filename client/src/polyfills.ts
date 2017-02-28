21dda252b8ba3ffcdc5d96497c6ac40685c3d0f9
Artem Kraskovskij <tema.kras@mail.ru>
2017-02-28 19:21:09 +0300

    refactor

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -1,14 +1,4 @@
 import 'core-js/client/shim';
 import 'reflect-metadata';
 require('zone.js/dist/zone');
-
 import 'ts-helpers';
-
-if (process.env.ENV === 'build') {
-  // Production
-
-} else {
-  // Development
-  Error['stackTraceLimit'] = Infinity;
-  require('zone.js/dist/long-stack-trace-zone');
-}
