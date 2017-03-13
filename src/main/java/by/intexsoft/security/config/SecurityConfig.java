b42c2d4c67999649e8575dccd62a5ce47df4d20c
akraskovski <artem.kraskovski@intexsoft.by>
2017-03-13 20:35:54 +0300

    some changes

--- src/main/java/by/intexsoft/security/config/SecurityConfig.java
+++ src/main/java/by/intexsoft/security/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/service/hello").hasAuthority("ROLE_ADMIN")
+                .antMatchers("/service/hello").hasAnyAuthority("ROLE_ADMIN", "ROLE_STOCK_MANAGER")
                 .and()
                 .addFilterBefore(new AuthenticationTokenFilter(tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
