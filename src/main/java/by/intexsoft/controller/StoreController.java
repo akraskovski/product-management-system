d7e6d5778f0b570fb0ad2233328be68850e9f99d
Artem Kraskovskij <tema.kras@mail.ru>
2017-03-16 22:59:08 +0300

    new api in store

--- src/main/java/by/intexsoft/controller/StoreController.java
+++ src/main/java/by/intexsoft/controller/StoreController.java
@@ -5,9 +5,7 @@ import by.intexsoft.service.StoreService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +30,48 @@ public class StoreController {
         } catch (NullPointerException e) {
             LOGGER.error("Exception in loadAllStores. " + e.getLocalizedMessage());
             return null;
+        }
+    }
+
+    /**
+     * Creating {@link Store} from client form
+     * @param store model
+     */
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public void createStore(@RequestBody Store store) {
+        LOGGER.info("Start createStore");
+        try {
+            storeService.create(store);
+        } catch (NullPointerException e) {
+            LOGGER.error("Exception in createStore. " + e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Update {@link Store} entity in database
+     * @param store model
+     */
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    public void updateStore(@RequestBody Store store) {
+        LOGGER.info("Start updateStore");
+        try {
+            storeService.update(store.id, store);
+        } catch (NullPointerException e) {
+            LOGGER.error("Exception in updateStore. " + e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Delete {@link Store} from database by identifier
+     * @param id
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteStore(@PathVariable("id") Integer id) {
+        LOGGER.info("Start deleteStore");
+        try {
+            storeService.delete(id);
+        } catch (NullPointerException e) {
+            LOGGER.error("Exception in deleteStore. " + e.getLocalizedMessage());
         }
     }
 }
