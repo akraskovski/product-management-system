2c64092409f99374ae9ef937b698620b5453a891
akraskovskij <artem.kraskovski@intexsoft.by>
2017-03-21 14:08:52 +0300

    login, logout links added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {AuthorizationService} from "./authorization/authorization.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'my-app',
@@ -6,4 +8,19 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    constructor(private router: Router, private authorizationService: AuthorizationService) {
+
+    }
+
+    isLoggedIn(): boolean {
+        return AuthorizationService.getCurrentUser();
+    }
+
+    logout(): void {
+        if (this.isLoggedIn()) {
+            this.authorizationService.logout();
+            this.router.navigate(["/"]);
+        }
+    }
 }
