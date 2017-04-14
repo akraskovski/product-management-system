a33c3998efab219cbd10b6c84c4043ed308f1dd1
akraskovskij <artem.kraskovski@intexsoft.by>
2017-04-14 14:03:06 +0300

    pagination started

--- client/src/app/product/content/product-content.component.ts
+++ client/src/app/product/content/product-content.component.ts
@@ -11,6 +11,13 @@ import {AuthorityWorker} from "../../common/authority-worker";
 })
 export class ProductContentComponent extends AuthorityWorker implements OnInit {
     productList: Product[];
+    filteredItems: Product[];
+    pages: number = 4;
+    pageSize: number = 5;
+    pageNumber: number = 0;
+    currentIndex: number = 1;
+    pagesIndex: Array<number>;
+    pageStart: number = 1;
 
     constructor(private productService: CommonService, private router: Router) {
         super();
@@ -20,11 +27,68 @@ export class ProductContentComponent extends AuthorityWorker implements OnInit {
         this.load();
     }
 
+    createPagination() {
+        this.currentIndex = 1;
+        this.pageStart = 1;
+        this.pages = 4;
+        this.pageNumber = parseInt("" + (this.filteredItems.length / this.pageSize));
+        if (this.filteredItems.length % this.pageSize != 0) {
+            this.pageNumber++;
+        }
+        if (this.pageNumber < this.pages) {
+            this.pages = this.pageNumber;
+        }
+        this.refreshItems();
+    }
+
+    refreshItems() {
+        this.productList = this.filteredItems.slice((this.currentIndex - 1) * this.pageSize, (this.currentIndex) * this.pageSize);
+        this.pagesIndex = this.fillArray();
+    }
+
+    fillArray(): any {
+        let obj = [];
+        for (let index = this.pageStart; index < this.pageStart + this.pages; index++) {
+            obj.push(index);
+        }
+        return obj;
+    }
+
+    prevPage() {
+        if (this.currentIndex > 1) {
+            this.currentIndex--;
+        }
+        if (this.currentIndex < this.pageStart) {
+            this.pageStart = this.currentIndex;
+        }
+        this.refreshItems();
+    }
+
+    nextPage() {
+        if (this.currentIndex < this.pageNumber) {
+            this.currentIndex++;
+        }
+        if (this.currentIndex >= (this.pageStart + this.pages)) {
+            this.pageStart = this.currentIndex - this.pages + 1;
+        }
+        this.refreshItems();
+    }
+
+    setPage(index: number) {
+        this.currentIndex = index;
+        this.refreshItems();
+    }
+
     private load(): void {
         this.productService.loadAll(api.PRODUCT)
             .subscribe(
-                productList => this.productList = productList,
-                err => this.logError(err));
+                productList => {
+                    this.productList = productList;
+                    this.filteredItems = productList;
+                    this.createPagination();
+                },
+                err => this.logError(err)
+            );
     }
 
     onDelete(id: number): void {
