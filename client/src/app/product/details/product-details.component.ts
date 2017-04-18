a8c916f9027b34ea7afd464d6c4fe5a5aa2d9565
akraskovskij <tema.kras@mail.ru>
2017-04-18 13:26:01 +0300

    product details page changes

--- client/src/app/product/details/product-details.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-    selector: 'product-details-component',
-    templateUrl: 'product-details.component'
-})
-export class ProductDetailsComponent {
-
-}
