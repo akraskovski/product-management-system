a60e2f3e5f44a5434c66debb6b24c8247138f3f2
akraskovskij <tema.kras@mail.ru>
2017-05-01 23:21:03 +0300

    dump file updated

--- src/main/resources/db/migration/V1__CreateStructure.sql
+++ src/main/resources/db/migration/V1__CreateStructure.sql
@@ -8,16 +8,12 @@ CREATE TABLE authority (
   id   INTEGER     NOT NULL,
   name AUTHORITIES NOT NULL
 );
-ALTER TABLE authority
-  OWNER TO postgres;
 CREATE SEQUENCE authority_id_seq
 START WITH 1
 INCREMENT BY 1
 NO MINVALUE
 NO MAXVALUE
 CACHE 1;
-ALTER TABLE authority_id_seq
-  OWNER TO postgres;
 ALTER SEQUENCE authority_id_seq OWNED BY authority.id;
 
 CREATE TABLE product (
@@ -25,26 +21,21 @@ CREATE TABLE product (
   name    CHARACTER VARYING(255) NOT NULL,
   cost    DOUBLE PRECISION       NOT NULL,
   type    CHARACTER VARYING(255),
-  details CHARACTER VARYING(500)
+  details CHARACTER VARYING(500),
+  image   CHARACTER VARYING(255)
 );
-ALTER TABLE product
-  OWNER TO postgres;
 CREATE SEQUENCE product_id_seq
 START WITH 1
 INCREMENT BY 1
 NO MINVALUE
 NO MAXVALUE
 CACHE 1;
-ALTER TABLE product_id_seq
-  OWNER TO postgres;
 ALTER SEQUENCE product_id_seq OWNED BY product.id;
 
 CREATE TABLE product_stock (
   product_id INTEGER NOT NULL,
   stock_id   INTEGER NOT NULL
 );
-ALTER TABLE product_stock
-  OWNER TO postgres;
 CREATE TABLE stock (
   id         INTEGER                NOT NULL,
   specialize CHARACTER VARYING(255) NOT NULL,
@@ -52,24 +43,18 @@ CREATE TABLE stock (
   phone      CHARACTER VARYING(50),
   square     DOUBLE PRECISION
 );
-ALTER TABLE stock
-  OWNER TO postgres;
 CREATE SEQUENCE stock_id_seq
 START WITH 1
 INCREMENT BY 1
 NO MINVALUE
 NO MAXVALUE
 CACHE 1;
-ALTER TABLE stock_id_seq
-  OWNER TO postgres;
 ALTER SEQUENCE stock_id_seq OWNED BY stock.id;
 
 CREATE TABLE stock_store (
   stock_id INTEGER NOT NULL,
   store_id INTEGER NOT NULL
 );
-ALTER TABLE stock_store
-  OWNER TO postgres;
 
 CREATE TABLE store (
   id        INTEGER                NOT NULL,
@@ -79,18 +64,15 @@ CREATE TABLE store (
   phone     CHARACTER VARYING(50),
   skype     CHARACTER VARYING(45),
   discounts BOOLEAN,
-  mail      CHARACTER VARYING(45)
+  mail      CHARACTER VARYING(45),
+  logo      CHARACTER VARYING(255)
 );
-ALTER TABLE store
-  OWNER TO postgres;
 CREATE SEQUENCE store_id_seq
 START WITH 1
 INCREMENT BY 1
 NO MINVALUE
 NO MAXVALUE
 CACHE 1;
-ALTER TABLE store_id_seq
-  OWNER TO postgres;
 ALTER SEQUENCE store_id_seq OWNED BY store.id;
 
 CREATE TABLE "user" (
@@ -98,23 +80,17 @@ CREATE TABLE "user" (
   username CHARACTER VARYING NOT NULL,
   password CHARACTER VARYING NOT NULL
 );
-ALTER TABLE "user"
-  OWNER TO postgres;
 
 CREATE TABLE user_authority (
   user_id      INTEGER NOT NULL,
   authority_id INTEGER NOT NULL
 );
-ALTER TABLE user_authority
-  OWNER TO postgres;
 CREATE SEQUENCE user_id_seq
 START WITH 1
 INCREMENT BY 1
 NO MINVALUE
 NO MAXVALUE
 CACHE 1;
-ALTER TABLE user_id_seq
-  OWNER TO postgres;
 ALTER SEQUENCE user_id_seq OWNED BY "user".id;
 
 ALTER TABLE ONLY authority
