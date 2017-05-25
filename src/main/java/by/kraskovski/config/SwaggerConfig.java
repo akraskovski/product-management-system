9e7f9cf4b345d7eb7164cccb625affdb28448195
akraskovski <artem.kraskovski@gmail.com>
2017-05-25 11:04:32 +0300

    added swagger support

---
+++ src/main/java/by/kraskovski/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package by.kraskovski.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+}
