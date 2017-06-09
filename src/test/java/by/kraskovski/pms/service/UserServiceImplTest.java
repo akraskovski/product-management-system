c429fc33fda707ba2439d2ebb76e560d918e743d
akraskovski <artem.kraskovski@gmail.com>
2017-06-09 10:38:37 +0300

    unit test updated

--- src/test/java/by/kraskovski/pms/service/UserServiceImplTest.java
+++ src/test/java/by/kraskovski/pms/service/UserServiceImplTest.java
@@ -2,12 +2,18 @@ package by.kraskovski.pms.service;
 
 import by.kraskovski.pms.model.User;
 import by.kraskovski.pms.service.UserService;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class UserServiceImplTest {
@@ -21,10 +27,30 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void find() {
+    public void findUserTest() {
         final int id = 10;
         when(userService.find(id)).thenReturn(new User(id, "Test", "Test"));
         User foundUser = userService.find(id);
         Assert.assertNotNull(foundUser);
+    }
+
+    @Test
+    public void updateUserTest() {
+        final User user = createUser();
+        when(userService.create(user)).thenReturn(user);
+        when(userService.update(user)).thenReturn(user);
+        userService.create(user);
+        user.setUsername("Updated user");
+        user.setPassword("Updated password");
+        userService.update(user);
+        verify(userService, times(1)).create(user);
+        verify(userService, times(1)).update(user);
+    }
+
+    private User createUser() {
+        final int id = RandomUtils.nextInt();
+        final String username = Arrays.toString(RandomUtils.nextBytes(20));
+        final String password = Arrays.toString(RandomUtils.nextBytes(20));
+        return new User(id, username, password);
     }
 }
