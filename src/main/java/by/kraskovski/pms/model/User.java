2329455e68a93fdda0172c92bd1c72fd118bd9c6
akraskovski <artem.kraskovski@gmail.com>
2017-06-23 16:18:01 +0300

    changed schema to public, created product test with date

--- src/main/java/by/kraskovski/pms/model/User.java
+++ src/main/java/by/kraskovski/pms/model/User.java
@@ -5,15 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
@@ -22,6 +14,7 @@ import java.util.List;
  * Description entity by database table "user"
  */
 @Entity
+@Table(name = "\"user\"")
 public class User extends BaseEntity implements Authentication {
 
     @ManyToMany(fetch = FetchType.EAGER)
---
+++ src/test/java/by/kraskovski/pms/repository/ProductRepositoryTest.java
@@ -0,0 +1,49 @@
+package by.kraskovski.pms.repository;
+
+import by.kraskovski.pms.Application;
+import by.kraskovski.pms.model.Product;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = Application.class)
+@DataJpaTest
+//@TestPropertySource("classpath:/application-test.properties")
+public class ProductRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void createProductTest() {
+        Product product = prepareProduct();
+        productRepository.save(product);
+        assertNotNull(product.getId());
+    }
+
+    private Product prepareProduct() {
+        Product product = new Product();
+        product.setName("Product");
+        product.setCost(420.5);
+        product.setDetails("Description of product details");
+        product.setHeight(391);
+        product.setWidth(102);
+        product.setWeight(10);
+        product.setType("Product Type");
+        product.setImage("--null--");
+        product.setManufactureDate(LocalDateTime.now());
+        return product;
+    }
+}
