3a06c1e58e5b3e3b5e7993609047383eeb3c463d
akraskovski <artem.kraskovski@gmail.com>
2017-07-02 16:25:21 +0300

    userService test refactored

--- server/src/test/java/by/kraskovski/pms/service/UserServiceTest.java
+++ server/src/test/java/by/kraskovski/pms/service/UserServiceTest.java
@@ -1,55 +1,69 @@
 package by.kraskovski.pms.service;
 
+import by.kraskovski.pms.domain.Authority;
 import by.kraskovski.pms.domain.User;
+import by.kraskovski.pms.domain.enums.AuthorityEnum;
+import by.kraskovski.pms.repository.UserRepository;
+import by.kraskovski.pms.service.impl.UserServiceImpl;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.Arrays;
-
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 
     @Mock
-    private UserService userService;
+    private UserRepository userRepository;
 
-    @Before
-    public void before() {
-        MockitoAnnotations.initMocks(this);
-    }
+    @Mock
+    private AuthorityService authorityService;
 
-    @Test
-    public void findUserTest() {
-        final int id = 10;
-        when(userService.find(id)).thenReturn(new User(id, "Test", "Test"));
-        final User foundUser = userService.find(id);
-        Assert.assertNotNull(foundUser);
-    }
+    @InjectMocks
+    private UserServiceImpl userService;
 
-    @Test
-    public void updateUserTest() {
-        final User user = createUser();
-        when(userService.create(anyObject())).thenReturn(user);
-        when(userService.update(anyObject())).thenReturn(user);
-        userService.create(user);
-        user.setUsername("Updated user");
-        user.setPassword("Updated password");
-        userService.update(user);
-        verify(userService).create(user);
-        verify(userService).update(user);
-    }
-
-    private User createUser() {
+    private User prepareUser() {
         final int id = RandomUtils.nextInt();
-        final String username = Arrays.toString(RandomUtils.nextBytes(20));
-        final String password = Arrays.toString(RandomUtils.nextBytes(20));
+        final String username = RandomStringUtils.random(20);
+        final String password = RandomStringUtils.random(20);
         return new User(id, username, password);
+    }
+
+    @Test
+    public void createTest() {
+        final User user = prepareUser();
+        when(authorityService.findByName(AuthorityEnum.ROLE_USER)).thenReturn(new Authority(AuthorityEnum.ROLE_USER));
+        when(userRepository.save((User) anyObject())).thenReturn(user);
+
+        assertNotEquals(userService.create(new User()).getId(), 0);
+        verify(authorityService).findByName(anyObject());
+        verify(userRepository).save((User) anyObject());
+    }
+
+    @Test
+    public void findByIdTest() {
+        final User user = prepareUser();
+        when(userRepository.findOne(anyInt())).thenReturn(user);
+
+        assertEquals(user, userService.find(user.getId()));
+        verify(userRepository).findOne(anyInt());
+    }
+
+    @Test
+    public void findByUsernameTest() {
+        final User user = prepareUser();
+        when(userRepository.findByUsername(anyString())).thenReturn(user);
+
+        assertEquals(user, userService.findByUsername(user.getUsername()));
+        verify(userRepository).findByUsername(anyString());
     }
 }
