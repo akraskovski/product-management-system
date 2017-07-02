2968c564e593eb28d27e24cb993d548a1eaf6ca2
akraskovski <artem.kraskovski@gmail.com>
2017-07-02 16:01:58 +0300

    product test finished

--- server/src/test/java/by/kraskovski/pms/service/ProductServiceTest.java
+++ server/src/test/java/by/kraskovski/pms/service/ProductServiceTest.java
@@ -11,8 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,14 +28,60 @@ public class ProductServiceTest {
     @InjectMocks
     private ProductServiceImpl productService;
 
-    @Test
-    public void createProductTest() {
-        final Product product = new Product(RandomUtils.nextInt(), RandomStringUtils.random(20));
-        doReturn(product).when(productRepository).save((Product) anyObject());
-        productService.create(product);
+    private Product prepareProduct() {
+        final Product product = new Product();
+        product.setId(RandomUtils.nextInt());
+        product.setName(RandomStringUtils.random(20));
+        product.setType(RandomStringUtils.random(20));
+        product.setImage(RandomStringUtils.random(40));
+        return product;
+    }
 
-        verify(productRepository).save(product);
-        assertNotEquals(product.getId(), 0);
-        assertNotNull(product.getManufactureDate());
+    @Test
+    public void createTest() {
+        final Product product = prepareProduct();
+        when(productRepository.save((Product) anyObject())).thenReturn(product);
+
+        assertNotEquals(productService.create(new Product()).getId(), 0);
+        verify(productRepository).save((Product) anyObject());
+    }
+
+    @Test
+    public void findByIdTest() {
+        final Product product = prepareProduct();
+        when(productRepository.findOne(anyInt())).thenReturn(product);
+
+        assertEquals(product, productService.find(product.getId()));
+        verify(productRepository).findOne(product.getId());
+    }
+
+    @Test
+    public void findByNameTest() {
+        final Product product = prepareProduct();
+        when(productRepository.findByName(anyString())).thenReturn(singletonList(product));
+
+        assertEquals(singletonList(product), productService.findByName(product.getName()));
+        verify(productRepository).findByName(anyString());
+    }
+
+    @Test
+    public void findByTypeTest() {
+        final Product product = prepareProduct();
+        when(productRepository.findByType(anyString())).thenReturn(singletonList(product));
+
+        assertEquals(singletonList(product), productService.findByType(product.getType()));
+        verify(productRepository).findByType(anyString());
+    }
+
+    @Test
+    public void deleteTest() {
+        final Product product = prepareProduct();
+        when(imageService.delete(anyString())).thenReturn(true);
+        when(productRepository.findOne(anyInt())).thenReturn(product);
+
+        productService.delete(product.getId());
+
+        verify(imageService).delete(anyString());
+        verify(productRepository).delete((Product) anyObject());
     }
 }
