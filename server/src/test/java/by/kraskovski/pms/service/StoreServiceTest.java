86cddcf644472535f6d6337e67da1c9c6aabfb2b
akraskovski <artem.kraskovski@gmail.com>
2017-08-23 14:26:43 +0300

    store service testing finished

--- server/src/test/java/by/kraskovski/pms/service/StoreServiceTest.java
+++ server/src/test/java/by/kraskovski/pms/service/StoreServiceTest.java
@@ -10,10 +10,12 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static by.kraskovski.pms.utils.TestUtils.prepareStore;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyObject;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StoreServiceTest {
@@ -29,10 +31,53 @@ public class StoreServiceTest {
 
     @Test
     public void createTest() {
-        final Store store = prepareStore();
-        when(storeRepository.save((Store) anyObject())).thenReturn(store);
+        when(storeRepository.save((Store) anyObject())).thenReturn(prepareStore());
 
         assertNotEquals(0, storeService.create(new Store()).getId());
         verify(storeRepository).save((Store) anyObject());
+    }
+
+    @Test
+    public void findByIdTest() {
+        final Store store = prepareStore();
+        when(storeRepository.findOne(anyInt())).thenReturn(store);
+
+        assertEquals(store, storeService.find(store.getId()));
+        verify(storeRepository).findOne(anyInt());
+    }
+
+    @Test
+    public void findByNameTest() {
+        final Store store = prepareStore();
+        when(storeRepository.findByName(anyString())).thenReturn(store);
+
+        assertEquals(store, storeService.findByName(store.getName()));
+        verify(storeRepository).findByName(anyString());
+    }
+
+    @Test
+    public void deleteStoreWithLogoTest() {
+        final Store store = prepareStore();
+        when(storeRepository.findOne(anyInt())).thenReturn(store);
+        when(imageService.delete(anyString())).thenReturn(true);
+
+        storeService.delete(store.getId());
+
+        verify(storeRepository).findOne(store.getId());
+        verify(imageService).delete(store.getLogo());
+        verify(storeRepository).delete(store.getId());
+    }
+
+    @Test
+    public void deleteStoreWithoutLogoTest() {
+        final Store store = prepareStore();
+        store.setLogo(org.apache.commons.lang3.StringUtils.EMPTY);
+        when(storeRepository.findOne(anyInt())).thenReturn(store);
+
+        storeService.delete(store.getId());
+
+        verify(storeRepository).findOne(store.getId());
+        verifyZeroInteractions(imageService);
+        verify(storeRepository).delete(store);
     }
 }
