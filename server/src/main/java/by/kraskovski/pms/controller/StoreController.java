4086bb43897a6038f6477ff426280a745955141f
akraskovski <artem.kraskovski@gmail.com>
2017-09-17 12:45:27 +0300

    #12 - checkstyle

--- server/src/main/java/by/kraskovski/pms/controller/StoreController.java
+++ server/src/main/java/by/kraskovski/pms/controller/StoreController.java
@@ -11,7 +11,12 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- server/src/main/java/by/kraskovski/pms/domain/model/Store.java
+++ server/src/main/java/by/kraskovski/pms/domain/model/Store.java
@@ -2,7 +2,6 @@ package by.kraskovski.pms.domain.model;
 
 import by.kraskovski.pms.domain.base.BaseEntity;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
