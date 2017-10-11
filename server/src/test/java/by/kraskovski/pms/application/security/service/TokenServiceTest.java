025a68937c337f130bab3f570acc9ed9078269ab
akraskovski <artem.kraskovski@gmail.com>
2017-10-11 18:21:58 +0300

    started security testing

---
+++ server/src/test/java/by/kraskovski/pms/application/security/service/TokenServiceTest.java
@@ -0,0 +1,79 @@
+package by.kraskovski.pms.application.security.service;
+
+import by.kraskovski.pms.application.security.exception.UserNotFoundException;
+import by.kraskovski.pms.domain.model.Authority;
+import by.kraskovski.pms.domain.model.User;
+import by.kraskovski.pms.domain.model.enums.AuthorityEnum;
+import by.kraskovski.pms.domain.service.AuthorityService;
+import by.kraskovski.pms.domain.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static by.kraskovski.pms.utils.TestUtils.prepareUserWithRole;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+
+/**
+ * Integration test for main security logic of application.
+ * Check generating and verifying JWT Tokens.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class TokenServiceTest {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private AuthorityService authorityService;
+
+    @Autowired
+    private UserService userService;
+
+    @Value("${secret.key:JKGuhygvuh2v}")
+    private String secretKey;
+
+    @Before
+    public void before() {
+        userService.deleteAll();
+        authorityService.deleteAll();
+    }
+
+    @Test
+    public void generateWithValidDataTest() throws Exception {
+        final User user = createUser();
+        final String token = tokenService.generate(user.getUsername(), user.getPassword()).getToken();
+        //TODO: Validate generated token.
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    public void generateIfUserNotExistsTest() throws Exception {
+        tokenService.generate(randomAlphabetic(5), randomAlphabetic(5));
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void generateIfEmptyPasswordTest() throws Exception {
+        final User user = createUser();
+        tokenService.generate(user.getUsername(), "");
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void generateIfPasswordNotEqualsTest() throws Exception {
+        final User user = createUser();
+        tokenService.generate(user.getUsername(), randomAlphabetic(50));
+    }
+
+    private User createUser() {
+        final Authority authority = new Authority(AuthorityEnum.ROLE_ADMIN);
+        authorityService.create(authority);
+        final User user = prepareUserWithRole(authority);
+        return userService.create(user);
+    }
+}
