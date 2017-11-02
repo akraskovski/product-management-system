010ca91a5836c3dcdb479c1e80df109d41cc7630
akraskovski <artem.kraskovski@gmail.com>
2017-11-02 21:00:48 +0300

    #-12 simplified controller mapping annotations

--- server/src/main/java/by/kraskovski/pms/application/controller/AuthorityController.java
+++ server/src/main/java/by/kraskovski/pms/application/controller/AuthorityController.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,7 +23,7 @@ public class AuthorityController {
     /**
      * Return json-information about all authorities in database.
      */
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity loadAuthorities() {
         log.info("Start loading all authorities");
         return ResponseEntity.ok(authorityService.findAll());
--- server/src/main/java/by/kraskovski/pms/application/controller/ProductController.java
+++ server/src/main/java/by/kraskovski/pms/application/controller/ProductController.java
@@ -9,10 +9,13 @@ import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,7 +38,7 @@ public class ProductController {
     /**
      * Find all products in database.
      */
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity loadAllProducts() {
         log.info("Start loadAllProducts");
         return ResponseEntity.ok(productService.findAll().stream()
@@ -46,7 +49,7 @@ public class ProductController {
     /**
      * Find products in database with setting id in browser.
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public ResponseEntity loadProductById(@PathVariable final String id) {
         log.info("Start loadProductById: {}", id);
         return ResponseEntity.ok(mapper.map(productService.find(id), ProductDto.class));
@@ -55,7 +58,7 @@ public class ProductController {
     /**
      * Find products in database with setting name in browser
      */
-    @RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
+    @GetMapping("/name/{name}")
     public ResponseEntity loadProductsByName(@PathVariable final String name) {
         log.info("Start loadProductsByName: {}", name);
         return ResponseEntity.ok(productService.findByName(name).stream()
@@ -66,7 +69,7 @@ public class ProductController {
     /**
      * Find products in database with setting type in browser.
      */
-    @RequestMapping(value = "/type/{type}", method = RequestMethod.GET)
+    @GetMapping("/type/{type}")
     public ResponseEntity loadProductsByType(@PathVariable final String type) {
         log.info("Start loadProductsByType: {}", type);
         return ResponseEntity.ok(productService.findByType(type).stream()
@@ -77,7 +80,7 @@ public class ProductController {
     /**
      * Creating {@link Product} from client form.
      */
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity createProduct(@RequestBody @Valid final ProductDto productDto) {
         log.info("Start createProduct: {}", productDto.getName());
         final Product product = productService.create(mapper.map(productDto, Product.class));
@@ -87,7 +90,7 @@ public class ProductController {
     /**
      * Update {@link Product}'s information in database.
      */
-    @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public ResponseEntity updateProduct(@RequestBody @Valid final ProductDto productDto) {
         log.info("start updateProduct: {}", productDto.getId());
         final Product product = productService.update(mapper.map(productDto, Product.class));
@@ -97,7 +100,7 @@ public class ProductController {
     /**
      * Delete {@link Product} from database by identifier.
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteProduct(@PathVariable final String id) {
         log.info("Start deleteProduct: {}", id);
