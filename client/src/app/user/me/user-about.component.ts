a2ecc1a50f9b3c9318c7b8662abb84e98e57fabc
akraskovski <artem.kraskovski@gmail.com>
2017-11-12 14:48:50 +0300

    add fucking user about module

---
+++ client/src/app/user/me/user-about.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from "@angular/core";
+import {User} from "../../model/user";
+import {UserService} from "../user.service";
+import {Router} from "@angular/router";
+
+@Component({
+    selector: 'user-about-component',
+    templateUrl: 'user-about.component.html'
+})
+export class UserAboutComponent implements OnInit{
+    user: User;
+
+    constructor(private userService: UserService, private router: Router) {
+    }
+
+    ngOnInit(): void {
+        this.userService.getCurrentUser()
+            .subscribe(userDto => this.user = userDto,
+                error => this.logError(error));
+    }
+
+    logError(error: Error): void {
+        console.error('There was an error: ' + error.message ? error.message : error.toString());
+        this.router.navigate(['/']);
+    }
+}
