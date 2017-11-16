e5459f10c454002b73760db340e38ab71d2f985e
akraskovski <artem.kraskovski@gmail.com>
2017-11-16 22:38:27 +0300

    started fucking user about edit page

---
+++ client/src/app/user/me/user-about-edit.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from "@angular/core";
+import {User} from "../../model/user";
+import {UserService} from "../user.service";
+import {Router} from "@angular/router";
+
+@Component({
+    selector: 'user-about-edit-component',
+    templateUrl: 'user-about-edit.component.html'
+})
+export class UserAboutEditComponent implements OnInit{
+    user: User;
+
+    constructor(private userService: UserService, private router: Router) {
+        this.user = new User();
+    }
+
+    ngOnInit(): void {
+        this.userService.getCurrentUser()
+            .subscribe(userDto => this.user = userDto,
+                error => this.logError(error));
+    }
+
+    logError(error: Error): void {
+        console.error('There was an error: ' + error.message ? error.message : error.toString());
+        this.router.navigate(['/']);
+    }
+}
