b50d5f1492ad953e1fd95e7817124e0a383c6cdb
akraskovski <artem.kraskovski@gmail.com>
2017-11-19 14:28:58 +0300

    user about edit page

--- client/src/app/user/me/user-about-edit.component.ts
+++ client/src/app/user/me/user-about-edit.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from "@angular/core";
 import {User} from "../../model/user";
 import {UserService} from "../user.service";
 import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {regex} from "../../constants/regex";
 
 @Component({
     selector: 'user-about-edit-component',
@@ -9,14 +11,43 @@ import {Router} from "@angular/router";
 })
 export class UserAboutEditComponent implements OnInit{
     user: User;
+    userForm: FormGroup;
 
     constructor(private userService: UserService, private router: Router) {
         this.user = new User();
     }
 
     ngOnInit(): void {
+        this.createEmptyForm();
+        this.fillForm();
+    }
+
+    private createEmptyForm(): void {
+        this.userForm = new FormGroup({
+            username: new FormControl('', Validators.required),
+            password: new FormControl('', [Validators.required, Validators.pattern(regex.PASSWORD)]),
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl('', Validators.pattern(regex.EMAIL)),
+            phone: new FormControl('', Validators.pattern(regex.PHONE_NUMBER)),
+            avatar: new FormControl()
+        });
+    }
+
+    private fillForm(): void {
         this.userService.getCurrentUser()
-            .subscribe(userDto => this.user = userDto,
+            .subscribe(
+                (userDto: User) => {
+                    this.user = userDto;
+                    this.userForm.setValue({
+                        username: this.user.username,
+                        password: this.user.password,
+                        firstName: this.user.firstName,
+                        lastName: this.user.lastName,
+                        email: this.user.email,
+                        phone: this.user.phone,
+                    });
+                },
                 error => this.logError(error));
     }
 
